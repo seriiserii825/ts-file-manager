30d2ba1e2a3ee3c6817d81931a3914846362655d
serii burduja <seriiburduja@gmail.com>
2025-10-23 15:58:37 +0300

    feat: updated ensure wp

--- src/modules/ensure/EnsureIsWp.ts
+++ src/modules/ensure/EnsureIsWp.ts
@@ -2,9 +2,9 @@ import chalk from "chalk";
 import fs from 'fs';
 import path from "path";
 export class EnsureIsWp {
-  constructor(baseDir: string) {
-    const style_file = path.join(baseDir, "style.css");
-    const functions_file = path.join(baseDir, "functions.php");
+  constructor(theme_dir: string) {
+    const style_file = path.join(theme_dir, "style.css");
+    const functions_file = path.join(theme_dir, "functions.php");
     if (!fs.existsSync(style_file) || !fs.existsSync(functions_file)) {
       console.log(chalk.red("The current directory is not a WordPress theme directory."));
       process.exit(1);
